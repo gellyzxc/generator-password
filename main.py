7c799f543449901e8c5d0881aafb7d739c36d443
gellyzxc <50029191+GeLLyQw@users.noreply.github.com>
2021-10-23 15:59:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
+import datetime
+import os
 
+version = "0.0.1"
 # Символы используемые для пароля.
 ARRAY_SYMBOLS = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -11,7 +14,7 @@ ARRAY_SYMBOLS = [
 # Получаем кол-во символов
 
 COUNT_SYMBOLS = 4
-
+print(f'Версия: {version}')
 input_count = input("Введите кол во символов в пароле: ")
 if input_count.isdigit():
     count_symbols = int(input_count)
@@ -23,6 +26,7 @@ def rand_symbols():
     return ARRAY_SYMBOLS[
         random.randint(0, len(ARRAY_SYMBOLS) - 1)
     ]
+
 
 print(f'Кол-во доступных символов: {len(ARRAY_SYMBOLS)}')
 # print(f'Доступные символы: {ARRAY_SYMBOLS}')
@@ -36,7 +40,25 @@ for i in range(0, count_symbols):
 
 print(f'Пароль: {password}')
 
-# Запись готового пароля в txt файл
+text_datetime = f'{datetime.datetime.now()}'
 
-with open('password.txt', 'a') as password_string:
+symbols_replace = ['-', ':', ' ', '.']
+filename = ''
+for s in text_datetime:
+    is_write = True
+    for sr in symbols_replace:
+        if s == sr:
+            filename += '_'
+            is_write = False
+
+    if is_write:
+        filename += s
+# Запись готового пароля в txt файл
+if not os.path.exists('passwords'):
+    os.mkdir('passwords')
+
+with open(f'passwords/{filename}_pswrd.txt', 'a') as password_string:
     password_string.write('{} \n'.format(f'{password}'))
+
+print(f'Нажмите клавишу ENTER чтобы закрыть')
+input()
